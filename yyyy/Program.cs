dab16ca490b519ac50863fc01373656798ef78ab
Ilona <ppsport88@mail.ru>
2023-02-23 19:59:15 +0300

    Task25

---
+++ yyyy/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Введите число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень В: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int numberC = 1;
+for (int i = 0; i < numberC; i++)
+{
+    Console.WriteLine($"Число А в степени В равно: " + Math.Pow(numberA, numberB));
+}
