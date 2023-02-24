fd20fc71657ef1c4859483672241354a54cfb834
Ilona <ppsport88@mail.ru>
2023-02-24 13:39:17 +0300

    Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+
+int[] GetBinaryArray(int size)
+{
+    int[] array = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(100);
+    }
+    return array;
+}
+int[] resultArray = GetBinaryArray(8);
+double [] test = new double[10];
+for (int i = 0; i < resultArray.Length; i++)
+{
+    Console.Write(resultArray[i] + "\t");
+}
